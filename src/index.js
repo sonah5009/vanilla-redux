ece1a56a5267e268f1e79c0e59005a841c0e18c1
sonah5009 <sonah5009@gmail.com>
2023-06-20 23:46:28 +0900

    1.2 Actions

--- src/index.js
+++ src/index.js
@@ -5,12 +5,27 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 // reducer function: modifying your data
-const countModifier = (count = 0) => {
-  //   console.log(count);
-  return count;
+// reducer 나 data modifier든 부르고 싶은대로 불러도 됨
+// count: state라고 생각
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 const countStore = createStore(countModifier);
 
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
+// countModifier = 0, { type: "HELLO"}
+// dispatch: sending messeage
 console.log(countStore.getState());
-console.log(countStore);
-// modifier & reducer의 return 값: 나의 application data
